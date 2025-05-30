4484ea497541420b122fa6e99897c15ace27a1b0
Morn Soda <mornsoda23@gmail.com>
2025-05-30 19:25:51 +0700

    added property booking class

--- src/models/booking/Booking.ts
+++ src/models/booking/Booking.ts
@@ -1,3 +1,17 @@
-class Booking {
-    
+import { BookingHistory } from "./BookingHistory";
+import { Cancellation } from "./Cancellation";
+import { Payment } from "./Payment";
+
+export class Booking {
+    constructor(
+        private id : string,
+        private userId : string,
+        private showtimeId : string,
+        private seats : string[],
+        private tickets :  Ticket[] = [],
+        private payment : Payment,
+        private BookingHistory : BookingHistory,
+        private cancellation? : Cancellation,
+
+    )
 }
