54f67c9854460e94e55e56d8f1e6ba72aeb17569
charyna-chab <charyna.chab@student.passerellesnumeriques.org>
2025-05-30 20:26:48 +0700

    feat(feature/showtime):added properties movieRoom and movie to class ShowTime

--- src/models/showtime/ShowTime.ts
+++ src/models/showtime/ShowTime.ts
@@ -1,6 +1,11 @@
+import { Movie } from "./Movie";
+import { MovieRoom } from "./MovieRoom";
+
 export class ShowTime{
     constructor(private startTime:string,
                 private endTime:string,
                 private price:number,
+                private movieRoom : MovieRoom,
+                private movie: Movie,
     ) {}
 }
