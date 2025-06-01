935d365288c1e2bd996da06f3e73a4ee518f2d87
unknown <mornsoda23@gmail.com>
2025-06-01 09:48:25 +0700

    added function applyDiscount in Promotion class

--- src/models/promotion/Promotion.ts
+++ src/models/promotion/Promotion.ts
@@ -5,5 +5,12 @@ export class Promotion {
         private discound : number,
         private descript : string,
         private isActive : boolean,
-    ){}
-}
+    ){
+        this.discound = discound >= 0 && discound < 100 ? discound : 0
+    }
+
+    applyDiscount(amount : number) : number{
+        if(amount <= 0 || !this.isActive) return amount;
+        return amount - (amount * this.discound / 100);
+    }
+}   
