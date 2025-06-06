7ffbeab85fd5ca6ed4bb24e41aeb69bc0be808a1
Morn Soda <mornsoda23@gmail.com>
2025-06-06 17:58:06 +0700

    cammand code of method in MovieManager class

--- src/models/showtime/MovieManager.ts
+++ src/models/showtime/MovieManager.ts
@@ -1,8 +1,16 @@
 import { Movie } from "./Movie";
-
 export class MovieManager {
+    /**
+     * Constructor to initialize the MovieManager with an optional array of movies.
+     * @param movies - Optional array of Movie objects.
+     */
     constructor(private movies: Movie[] = []) {}
 
+    /**
+     * Adds a new movie to the movie list.
+     * @param movie - A Movie object to be added.
+     * Throws an error if movie is not provided.
+     */
     addMovie(movie: Movie): void {
         if (!movie) {
             throw new Error("Movie is required");
@@ -10,6 +18,12 @@ export class MovieManager {
         this.movies.push(movie);
     }
 
+    /**
+     * Filters the movie list by a specific genre.
+     * @param genre - The genre to filter movies by.
+     * @returns An array of Movie objects matching the provided genre.
+     * Throws an error if genre is not provided.
+     */
     filterMoviesByGenre(genre: string): Movie[] {
         if (!genre) {
             throw new Error("Genre is required");
@@ -17,6 +31,10 @@ export class MovieManager {
         return this.movies.filter(movie => movie.matchesGenre(genre));
     }
 
+    /**
+     * Retrieves the full list of movies.
+     * @returns A new array containing all Movie objects.
+     */
     getMovies(): Movie[] {
         return [...this.movies];
     }
